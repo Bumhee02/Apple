d0e76ae799faa73492710ca56754cc6c2c514b33
Bumhee02 <rlaqjagml08@gmail.com>
2026-01-06 15:37:14 +0900

    commit

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,4 +1,7 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using TMPro;
 
 public class GameController : MonoBehaviour
@@ -7,8 +10,16 @@ public class GameController : MonoBehaviour
 	private	GameObject		panelMainMenu;
 	[SerializeField]
 	private	GameObject		panelInGame;
+	[SerializeField]
+	private	GameObject		panelGameOver;
     [SerializeField]
 	private	TextMeshProUGUI	textInGameScore;
+	[SerializeField]
+	private	TextMeshProUGUI	textGameOverScore;
+	[SerializeField]
+	private	Image			timeGauge;
+	[SerializeField]
+	private	float			maxTime = 120f;
     private	int				currentScore = 0;
     private	AudioSource		audioSource;
 
@@ -27,10 +38,39 @@ public class GameController : MonoBehaviour
 		panelInGame.SetActive(true);
 		audioSource.Play();
 
+		StartCoroutine(nameof(TimeCounter));
+
     }
     public void IncreaseScore(int addScore)
 	{
 		currentScore += addScore;
 		textInGameScore.text = currentScore.ToString();
+	}
+	public void ButtonRestart()
+	{
+		SceneManager.LoadScene(0);
+		//SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
+	public void GameOver()
+	{
+		textGameOverScore.text = $"SCORE\n{currentScore}";
+
+		panelInGame.SetActive(false);
+		panelGameOver.SetActive(true);
+		audioSource.Stop();
+	}
+	private IEnumerator TimeCounter()
+	{
+		float currentTime = maxTime;
+
+		while ( currentTime > 0 )
+		{
+			currentTime -= Time.deltaTime;
+			timeGauge.fillAmount = currentTime / maxTime;
+
+			yield return null;
+		}
+
+		GameOver();
 	}
 }
